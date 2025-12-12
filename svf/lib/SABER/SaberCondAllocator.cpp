66dc8a33ef687f7d0e7c905390d391f9bee0c666
Frank <211250215@smail.nju.edu.cn>
2025-12-12 22:33:13 +0800

    fix: 修正空指针条件判断
    通用修复：
    - 在 SaberCondAllocator::evaluateTestNullLikeExpr 中新增空指针检查：
      当 condVar->getICFGNode() == nullptr 时，返回 Condition::nullExpr()
      以避免在检查 memcached 时出现的异常情况。

--- svf/lib/SABER/SaberCondAllocator.cpp
+++ svf/lib/SABER/SaberCondAllocator.cpp
@@ -195,6 +195,10 @@ SaberCondAllocator::evaluateTestNullLikeExpr(const BranchStmt *branchStmt, const
         //  br i1 false, label %44, label %75, !dbg !7669 { "ln": 2033, "cl": 7, "fl": "re_lexer.c" }
         return Condition::nullExpr();
     }
+
+    // 额外修改；经测试uaf/uninit有的地方会因为此处出现nullptr报出assertion错误，提前进行检查，nullExpr表示无法判断
+    if(condVar->getICFGNode() == nullptr) return Condition::nullExpr();
+
     if (isTestNullExpr(SVFUtil::cast<ICFGNode>(condVar->getICFGNode())))
     {
         // succ is then branch
